5540ee52249fb05d94d5518f395f34915a7de0fc
Duxcoder <iinegve_25@mail.ru>
2023-01-28 19:06:08 +0300

    task complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
+        this.max = 0;
+        this.min = 0;
+        this.number = 0;
     }
 
-    guess() {
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
+        this.number = this.search();
+    }
+    
+    search() { return Math.round((this.max + this.min) / 2)}
 
+    guess() {
+       return this.number
     }
 
     lower() {
-
+        this.max = this.number
+        this.number = this.search()
     }
 
     greater() {
-
+        this.min = this.number
+        this.number = this.search()
     }
 }
 
